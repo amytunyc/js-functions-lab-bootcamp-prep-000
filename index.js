c7cf69dcfda6a0d24f269934a1ade4e438488830
Amy Tu <siamesebb@gmail.com>
2018-04-11 06:09:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  return `Happy holidays, ${name}`
+  return `Happy holidays, ${name}!`
 }
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days)
